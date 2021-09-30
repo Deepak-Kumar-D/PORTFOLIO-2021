9e1f4a14f6134ead0fc8666d538c6bfa407559ee
Deepak Kumar D <deepak.kumard36@gmail.com>
2021-10-01 00:27:35 +0530

    tooltip added, server links added

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ProjectDB from "../DB/Projects";
 import {
   FaGithub,
   FaReact,
@@ -16,408 +17,80 @@ function Project() {
     <section id="projects">
       <div className="projects-container">
         <div className="list-container">
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/Hackathon-Task-Pizza-Delivery"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
+          {ProjectDB.map((projectList) => {
+            return (
+              <div className="hub">
+                <div className="project-img">
+                  <div className="links">
+                    <a
+                      href={projectList.client}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaGithub className="client" />
+                      <span>Client</span>
+                    </a>
 
-                <a
-                  href="https://pizza-town.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
+                    {projectList.server ? (
+                      <a
+                        href={projectList.client}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <FaGithub className="server" />
+                        <span>Server</span>
+                      </a>
+                    ) : (
+                      <></>
+                    )}
+
+                    <a href={projectList.host} target="_blank" rel="noreferrer">
+                      <MdOpenInNew className="host" />
+                      <span>Demo</span>
+                    </a>
+                  </div>
+                  <img src={projectList.src} alt={projectList.alt} />
+                </div>
+
+                <p>{projectList.name}</p>
+
+                {projectList.tools === "fullstack" ? (
+                  <div className="tools">
+                    <DiMongodb />
+                    <span>Ex</span>
+                    <FaReact />
+                    <FaNodeJs />
+                    <FaHtml5 />
+                    <FaCss3Alt />
+                    <SiJavascript />
+                  </div>
+                ) : projectList.tools === "react" ? (
+                  <div className="tools">
+                    <FaReact />
+                    <FaNodeJs />
+                    <FaHtml5 />
+                    <FaCss3Alt />
+                    <SiJavascript />
+                  </div>
+                ) : projectList.tools === "react-firebase" ? (
+                  <div className="tools">
+                    <FaReact />
+                    <SiFirebase />
+                    <FaNodeJs />
+                    <FaHtml5 />
+                    <FaCss3Alt />
+                    <SiJavascript />
+                  </div>
+                ) : (
+                  <div className="tools">
+                    <FaHtml5 />
+                    <FaCss3Alt />
+                    <SiJavascript />
+                  </div>
+                )}
               </div>
-              <img src="/images/deployed/pizza.PNG" alt="pizza" />
-            </div>
-
-            <p>Pizza Town</p>
-
-            <div className="tools">
-              <DiMongodb />
-              <span>Ex</span>
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/Urlshortener-Frontend"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://react-urlshortener.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/url.PNG" alt="url" />
-            </div>
-
-            <p>URL Shortener</p>
-
-            <div className="tools">
-              <DiMongodb />
-              <span>Ex</span>
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/Assign-Mentor-Client"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://react-assign-mentor.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/mentor.PNG" alt="mentor" />
-            </div>
-
-            <p>Assign Mentor</p>
-
-            <div className="tools">
-              <DiMongodb />
-              <span>Ex</span>
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/React-Recipe-App"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://react-just-recipe.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/recipe.PNG" alt="recipe" />
-            </div>
-
-            <p>Just Recipe</p>
-
-            <div className="tools">
-              <DiMongodb />
-              <span>Ex</span>
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/React-Currency-Exchange"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://react-currency-exchanger.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/currency.PNG" alt="currency" />
-            </div>
-
-            <p>Currency Exchange</p>
-
-            <div className="tools">
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/Whatsapp-Clone-2021"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://whatsapp-clone-mern-2021.web.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/whatsapp.PNG" alt="whatsapp" />
-            </div>
-
-            <p>WhatsApp Clone</p>
-
-            <div className="tools">
-              <FaReact />
-              <SiFirebase />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/React-Users-Client"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://react-reactusers.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/profile.PNG" alt="profile" />
-            </div>
-
-            <p>User Profiles</p>
-
-            <div className="tools">
-              <DiMongodb />
-              <span>Ex</span>
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/Admin-Theme"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://react-admin-theme.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/admin.PNG" alt="admin" />
-            </div>
-
-            <p>Admin Theme</p>
-
-            <div className="tools">
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/React-Phone-Number-Validator"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://react-phone-number-validator.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/phone.PNG" alt="phone" />
-            </div>
-
-            <p>Phone Number Validator</p>
-
-            <div className="tools">
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/Hackathon-Task-GITHUB-CLONE"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://deepak-kumar-d.github.io/Hackathon-Task-GITHUB-CLONE/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/github.PNG" alt="github" />
-            </div>
-
-            <p>GitHub Clone</p>
-
-            <div className="tools">
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/Shopping-in-Style"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://shopping-in-style.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/shop.PNG" alt="shop" />
-            </div>
-
-            <p>Shopping In Style</p>
-
-            <div className="tools">
-              <FaReact />
-              <FaNodeJs />
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
-
-          <div className="hub">
-            <div className="project-img">
-              <div className="links">
-                <a
-                  href="https://github.com/Deepak-Kumar-D/Task-10_DOM-Task-1"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGithub />
-                </a>
-
-                <a
-                  href="https://deepak-kumar-d.github.io/Task-10_DOM-Task-1/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MdOpenInNew />
-                </a>
-              </div>
-              <img src="/images/deployed/calculator.PNG" alt="calculator" />
-            </div>
-
-            <p>Calculator</p>
-
-            <div className="tools">
-              <FaHtml5 />
-              <FaCss3Alt />
-              <SiJavascript />
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
 
