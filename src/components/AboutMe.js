1b0a90920385c1f27f42e68b9d097618e5ff94d7
Deepak Kumar D <deepak.kumard36@gmail.com>
2021-10-22 20:55:31 +0530

    mentor pic updated

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,16 +16,19 @@ function AboutMe() {
             <p>
               Hello! This is <span>Deepak Kumar D.</span>
               <br />
-              He is a FULL STACK DEVELOPER who is specialized in MERN Stack. He
-              has done a Full Stack Course from GUVI Geek Network Pvt Ltd. with
-              working demo projects (Don't Forget to check out the Projects
-              section). FrontEnd or BackEnd, the flexibility and the
-              accessibility is the key area where he always tries to focus.
+              He is a <strong>FULL STACK DEVELOPER</strong> who is specialized
+              in <strong>MERN</strong> Stack. He has done a Full Stack Course
+              from GUVI Geek Network Pvt Ltd. with working demo projects.{" "}
+              <small>
+                (Don't Forget to check out the <a href="#projects">Projects</a>{" "}
+                section)
+              </small>
               <br />
-              His steps have always been on the path of Creativity up till now.
-              He always work to present something attractive to the eyes. His
-              punctuality, dedication and team-effort with a pinch of necessary
-              leadership quality helps him in accomplishing the goals.
+              FrontEnd or BackEnd, the flexibility and the accessibility is the
+              key area where he always tries to focus.
+              <br />
+              His punctuality, dedication and team-effort with a pinch of
+              necessary leadership quality helps him in accomplishing the goals.
               <br />
               Maybe you also want to know that he has completed his degree in{" "}
               <strong>
